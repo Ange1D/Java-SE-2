844fb152c257d321cda2af5e74b58a7c5397d191
Ange1D <47231898+Ange1D@users.noreply.github.com>
2023-03-30 20:59:54 -0600

    S2E1

---
+++ e2/src/main/java/reto1/Main.java
@@ -0,0 +1,13 @@
+package reto1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Hilo1 h1 = new Hilo1();
+        Hilo2 h2 = new Hilo2();
+        h2.setDaemon(true);
+
+        h1.start();
+        h2.start();
+    }
+}
